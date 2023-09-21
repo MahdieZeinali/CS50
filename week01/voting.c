56863990161c3cc592ca6bbacd9d440b977ed16c
MahdieZeinali <mahdie13.zeinali76@gmail.com>
2023-09-21 14:00:09 +0200

    create voting system

---
+++ week01/voting.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char answer = get_char("do you agree?/n");
+
+    if(answer == 'y' || answer == 'Y')
+    {
+        printf("Agreed!/n");
+    }
+    else if(answer == 'n' || answer == 'N')
+    {
+        printf("Disagreed!/n");
+    }
+    else
+    {
+        printf("invalid input!/n");
+    }
+}
